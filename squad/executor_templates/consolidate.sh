8fd7eb4a9082d6c5bbaa03971d5665e6f2c1f8e9
jackalhan <jackalhan@gmail.com>
2018-12-31 03:45:11 -0600

    -some helper execution files are added

--- squad/executor_templates/consolidate.sh
+++ squad/executor_templates/consolidate.sh
@@ -1,1 +1,1 @@
-python ../Consolidate_Dev_Train_Document_Pairs.py --label_path="../DATA_SHORT_SQUAD/" --embedding_path="../DATA_SHORT_SQUAD/consolidated/" --partition_size=1000
+python ../Consolidate_Dev_Train_Document_Pairs.py --label_path="/media/jackalhan/Extreme 500/Rethinking_Running_Data_MSMARCO/" --embedding_path="/media/jackalhan/Extreme 500/Rethinking_Running_Data_MSMARCO/consolidated/" --partition_size=1000
